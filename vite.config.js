42b7d24eb270efa2ce9800a7fce684dbafb1eb42
lawliet7946130852@gmail.com <lawliet7946130852@gmail.com>
2026-03-24 15:18:14 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kotoba-23-app/', // <-- Tambahkan baris ini agar sesuai dengan nama repository
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
